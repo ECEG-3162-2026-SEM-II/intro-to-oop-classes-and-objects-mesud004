afe7d438d61c6a559e5f3aaae1d5763f8e8f3800
mesud004 <mesudetii660@gmail.com>
2026-03-15 13:56:43 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student s1 = new Student("Ahmed", 101);
+        // Student s1 = new Student("Ahmed", 101);
         Student s2 = new Student("Ali", 102);
 
-        System.out.println(s1.getName());
+        // System.out.println(s1.getName());
         System.out.println(s2.getName());
     }
 }
---
+++ src/student.java
@@ -0,0 +1,34 @@
+public class Student {
+
+    // private fields
+    private String name;
+    private int studentId;
+
+    // constructor
+    public Student(String name, int studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    // getter for name
+    public String getName() {
+        return name;
+    }
+
+    // setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // getter for studentId
+    public int getStudentId() {
+        return studentId;
+    }
+
+    // setter for studentId
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
+    
+
